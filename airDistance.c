c2738706d4c44da733a5577b260a5f83e998672c
hifza <hifzanazir456@gmail.com>
2023-10-20 20:33:04 +0500

    variable declaration, initialization...

--- airDistance.c
+++ airDistance.c
@@ -1,3 +1,9 @@
 #include<stdio.h>
 #include<math.h>
 double Degree_to_Radian(double value);
+{
+    double lon1,lati1,lon2,lati2;
+    double distance = 0 , delta = 0;
+    const int RADIUS = 6371;
+    
+}
