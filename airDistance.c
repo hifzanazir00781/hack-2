3c0ee1d9e61c2506c329b573b44b06c4834453c3
hifza <hifzanazir456@gmail.com>
2023-10-20 20:45:41 +0500

    call the function

--- airDistance.c
+++ airDistance.c
@@ -14,4 +14,9 @@ double Degree_to_Radian(double value);
     scanf("%lf",&lati2);
     printf("Enter the longitude 1 in degree -180 to 180 : ");
     scanf("%lf",&lon1);
+    
+    lon1 = Degree_to_Radian(lon1);
+    lati1= Degree_to_Radian(lati1);
+    lon2 = Degree_to_Radian(lon2);
+    lati2 = Degree_to_Radian(lati2);
 }
