02ed9ef69bd766105d01e310592565dc8fb24bb6
hifza <hifzanazir456@gmail.com>
2023-10-20 20:50:44 +0500

    formulas for delta and distance

--- airDistance.c
+++ airDistance.c
@@ -14,9 +14,14 @@ double Degree_to_Radian(double value);
     scanf("%lf",&lati2);
     printf("Enter the longitude 1 in degree -180 to 180 : ");
     scanf("%lf",&lon1);
-    
+
     lon1 = Degree_to_Radian(lon1);
     lati1= Degree_to_Radian(lati1);
     lon2 = Degree_to_Radian(lon2);
     lati2 = Degree_to_Radian(lati2);
+
+    delta = lon2 - lon1;
+    distance = (acos(sin(lati1))*sin(lati2)+cos(lati2)*cos(lati2)*cos(delta))*RADIUS;
+
+    
 }
