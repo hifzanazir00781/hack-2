475d1bfb30418513a82428797b91f780b6a7d5ad
hifza <hifzanazir456@gmail.com>
2023-10-20 20:39:51 +0500

    prompt the values from user

--- airDistance.c
+++ airDistance.c
@@ -5,5 +5,13 @@ double Degree_to_Radian(double value);
     double lon1,lati1,lon2,lati2;
     double distance = 0 , delta = 0;
     const int RADIUS = 6371;
-    
+
+    printf("Enter the latitude 1 in degree -90 to 90 : ");
+    scanf("%lf",&lati1);
+    printf("Enter the longitude 1 in degree -180 to 180 : ");
+    scanf("%lf",&lon1);
+    printf("Enter the latitude 2 in degree -90 to 90 : ");
+    scanf("%lf",&lati2);
+    printf("Enter the longitude 1 in degree -180 to 180 : ");
+    scanf("%lf",&lon1);
 }
