f35eaa3b620cfe0c1e449169eba597fa7ab84e68
hifza <hifzanazir456@gmail.com>
2023-10-21 11:45:40 +0500

    correct the error also hack 2 completed

--- airDistance.c
+++ airDistance.c
@@ -1,6 +1,15 @@
+/**
+ * Author:Hifza Nazir
+ * Email: hifzanazir456@gmail.com
+ * 
+ * program for interactively prompts the user to enter the latitude and longitude
+ * of two locations and then computes the distance between them using the formula.
+ * distance = (acos(sin(lati1)*sin(lati2)+cos(lati2)*cos(lati2)*cos(delta))*RADIUS);
+*/
 #include<stdio.h>
 #include<math.h>
 double Degree_to_Radian(double value);
+int main()
 {
     double lon1,lati1,lon2,lati2;
     double distance = 0 , delta = 0;
@@ -21,9 +30,11 @@ double Degree_to_Radian(double value);
     lati2 = Degree_to_Radian(lati2);
 
     delta = lon2 - lon1;
-    distance = (acos(sin(lati1))*sin(lati2)+cos(lati2)*cos(lati2)*cos(delta))*RADIUS;
+    distance = (acos(sin(lati1)*sin(lati2)+cos(lati2)*cos(lati2)*cos(delta))*RADIUS);
 
     printf("Location Distance\n");
+    printf("================\n");
+
     printf("origin = %lf %lf\n",lon1,lati1);
     printf("Distination = %lf %lf\n",lon2,lati2);
     printf("Distance = %lf\n",distance);
